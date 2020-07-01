41c0f3055d3f63fd35d79168ddf4a91c939a0820
Yoonseo Nam <turtlenam0928@gmail.com>
2020-07-01 13:09:53 +0900

    feat : 블록에 해싱 기능 추가
    - crypto-js 모듈 추가

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,18 @@
+import * as CryptoJS from 'crypto-js';
+
 class Block {
     public index: number;
     public hash: string;
     public previousHash: string;
     public data: string;
     public timestamp: number;
+
+    static calculateBlockHash = (
+        index: number,
+        previousHash: string,
+        timestamp: number,
+        data: string
+    ): string => CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
 
     constructor(
         index: number,
@@ -23,3 +32,9 @@ class Block {
 const genesisBlock: Block = new Block(0, "202020202020", "", "Hello", 123456);
 
 let blockchain: Block[] = [genesisBlock];
+
+const getBlockchain = (): Block[] => blockchain;
+
+const getLatestBlock = (): Block => blockchain[blockchain.length - 1];
+
+const getNewTimeStamp = (): number => Math.round(new Date().getTime() / 1000);
