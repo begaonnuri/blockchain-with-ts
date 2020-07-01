bdb700c1f963330f2c98046b6d61ae1b48299dce
Yoonseo Nam <turtlenam0928@gmail.com>
2020-07-01 13:45:41 +0900

    refactor : prettier 적용

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,33 @@
-import * as CryptoJS from 'crypto-js';
+import * as CryptoJS from "crypto-js";
 
 class Block {
-    public index: number;
-    public hash: string;
-    public previousHash: string;
-    public data: string;
-    public timestamp: number;
+  public index: number;
+  public hash: string;
+  public previousHash: string;
+  public data: string;
+  public timestamp: number;
 
-    static calculateBlockHash = (
-        index: number,
-        previousHash: string,
-        timestamp: number,
-        data: string
-    ): string => CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+  constructor(
+    index: number,
+    hash: string,
+    previousHash: string,
+    data: string,
+    timestamp: number
+  ) {
+    this.index = index;
+    this.hash = hash;
+    this.previousHash = previousHash;
+    this.data = data;
+    this.timestamp = timestamp;
+  }
 
-    constructor(
-        index: number,
-        hash: string,
-        previousHash: string,
-        data: string,
-        timestamp: number
-    ) {
-        this.index = index;
-        this.hash = hash;
-        this.previousHash = previousHash;
-        this.data = data;
-        this.timestamp = timestamp;
-    }
+  static calculateBlockHash = (
+    index: number,
+    previousHash: string,
+    timestamp: number,
+    data: string
+  ): string =>
+    CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
 }
 
 const genesisBlock: Block = new Block(0, "202020202020", "", "Hello", 123456);
@@ -40,9 +41,14 @@ const getLatestBlock = (): Block => blockchain[blockchain.length - 1];
 const getNewTimeStamp = (): number => Math.round(new Date().getTime() / 1000);
 
 const createNewBlock = (data: string): Block => {
-    const previousBlock: Block = getLatestBlock();
-    const newIndex: number = previousBlock.index + 1;
-    const nextTimestamp: number = getNewTimeStamp();
-    const nextHash: string = Block.calculateBlockHash(newIndex, previousBlock.hash, nextTimestamp, data);
-    return new Block(newIndex, nextHash, previousBlock.hash, data, nextTimestamp);
-}
+  const previousBlock: Block = getLatestBlock();
+  const newIndex: number = previousBlock.index + 1;
+  const nextTimestamp: number = getNewTimeStamp();
+  const nextHash: string = Block.calculateBlockHash(
+    newIndex,
+    previousBlock.hash,
+    nextTimestamp,
+    data
+  );
+  return new Block(newIndex, nextHash, previousBlock.hash, data, nextTimestamp);
+};
